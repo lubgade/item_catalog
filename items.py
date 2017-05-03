b667d327eafb10530fe34b13cd9e1c5729f4e605
Leena <lubgade@yahoo.com>
2017-05-02 23:47:47 -0400

    update items.py(add images)

--- items.py
+++ items.py
@@ -5,8 +5,8 @@ from database_setup import Categories, Items, Base
 
 engine = create_engine('sqlite:///jewelrydb.db')
 
-#Base.metadata.drop_all(engine)
-#Base.metadata.create_all(engine)
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
 
 Base.metadata.bind = engine
 
@@ -21,19 +21,19 @@ session.add(category1)
 session.commit()
 
 item1 = Items(name="Red Coral beads necklace", description="Vibrant and lively coral bead necklace", price="$29.99",
-              category=category1, picture="")
+              category=category1, picture="images/necklace.jpg")
 
 session.add(item1)
 session.commit()
 
 item2 = Items(name="Amethyst necklace", description="Beautiful amethyst necklace", price="15.99", category=category1,
-              picture="")
+              picture="images/necklace.jpg")
 
 session.add(item2)
 session.commit()
 
 item3 = Items(name="Black Onyx necklace", description="Features the shiny & pure blackness of black onyx",
-              price="$15.99", category=category1, picture="")
+              price="$15.99", category=category1, picture="images/necklace.jpg")
 session.add(item3)
 session.commit()
 
@@ -44,12 +44,12 @@ session.add(category2)
 session.commit()
 
 item1 = Items(name="black lava bead", description="natural black lava beads 8-10inch", price="$12.99", category=category2,
-              picture="")
+              picture="images/necklace.jpg")
 session.add(item1)
 session.commit()
 
 item2 = Items(name="Macrame bracelet", description="beautiful handmade Macrame bracelet", price="$10.99",
-              category=category2)
+              category=category2, picture="images/necklace.jpg")
 session.add(item2)
 session.commit()
 
@@ -59,12 +59,12 @@ session.add(category1)
 session.commit()
 
 item1 = Items(name="Pearl earrings", description="natural fresh water pearl earrings", price="$12.99",
-              category=category3)
+              category=category3, picture="images/necklace.jpg")
 session.add(item1)
 session.commit()
 
 item2 = Items(name="Red coral earrings", description="handmade red coral dangle earrings", price="$13.50",
-              category=category3)
+              category=category3, picture="images/necklace.jpg")
 session.add(item2)
 session.commit()
 
