a63c3d0def091f98153c9ff3586f947db2a08ddd
Leena <lubgade@yahoo.com>
2017-05-06 18:43:13 -0400

    update

--- items.py
+++ items.py
@@ -21,19 +21,20 @@ session.add(category1)
 session.commit()
 
 item1 = Items(name="Red Coral beads necklace", description="Vibrant and lively coral bead necklace", price="$29.99",
-              category=category1, picture="images/necklace.jpg")
+              category_id=category1.id, picture="static/images/necklace1.jpg")
 
 session.add(item1)
 session.commit()
 
-item2 = Items(name="Amethyst necklace", description="Beautiful amethyst necklace", price="15.99", category=category1,
-              picture="images/necklace.jpg")
+item2 = Items(name="Amethyst necklace", description="Beautiful amethyst necklace", price="15.99",
+              category_id=category1.id,
+              picture="static/images/necklace2.jpg")
 
 session.add(item2)
 session.commit()
 
 item3 = Items(name="Black Onyx necklace", description="Features the shiny & pure blackness of black onyx",
-              price="$15.99", category=category1, picture="images/necklace.jpg")
+              price="$15.99", category_id=category1.id, picture="static/images/necklace.jpg")
 session.add(item3)
 session.commit()
 
@@ -43,13 +44,14 @@ category2 = Categories(name="Bracelets", picture="images/necklace.jpg")
 session.add(category2)
 session.commit()
 
-item1 = Items(name="black lava bead", description="natural black lava beads 8-10inch", price="$12.99", category=category2,
-              picture="images/necklace.jpg")
+item1 = Items(name="black lava bead", description="natural black lava beads 8-10inch", price="$12.99",
+              category_id=category2.id,
+              picture="static/images/necklace.jpg")
 session.add(item1)
 session.commit()
 
 item2 = Items(name="Macrame bracelet", description="beautiful handmade Macrame bracelet", price="$10.99",
-              category=category2, picture="images/necklace.jpg")
+              category_id=category2.id, picture="static/images/necklace.jpg")
 session.add(item2)
 session.commit()
 
@@ -59,12 +61,12 @@ session.add(category1)
 session.commit()
 
 item1 = Items(name="Pearl earrings", description="natural fresh water pearl earrings", price="$12.99",
-              category=category3, picture="images/necklace.jpg")
+              category_id=category3.id, picture="static/images/necklace.jpg")
 session.add(item1)
 session.commit()
 
 item2 = Items(name="Red coral earrings", description="handmade red coral dangle earrings", price="$13.50",
-              category=category3, picture="images/necklace.jpg")
+              category_id=category3.id, picture="static/images/necklace.jpg")
 session.add(item2)
 session.commit()
 
