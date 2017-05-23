8db39db4853ae2c1992de0237663a51155b811de
Leena <lubgade@yahoo.com>
2017-05-23 13:17:52 -0400

    update

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,9 @@ $(document).on('change', ':file', function(){
 var input = $(this),
     numFiles = input.get(0).files ? input.get(0).files.length:1,
     label = input.val().replace(/\\/g, '/').replace(/.*\//,'');
-    input.trigger('fileselect',[numFiles,label])
+    input.trigger('fileselect',[numFiles,label]);
  });
+
 
 $(document).ready(function(){
     $(':file').on('fileselect',function(event,numFiles,label){
